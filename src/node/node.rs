7f5d43b8f2a53894d4ca2e0362abf3c3561a9f2c
DylanCoon99 <dylcoon99@gmail.com>
2024-03-11 16:15:55 -0400

    implemented basic node methods

--- src/node/node.rs
+++ src/node/node.rs
@@ -1,7 +1,7 @@
 use std::net::SocketAddr;
 use tokio::sync::{mpsc, Mutex};
 use std::collections::HashSet;
-use std::sync::Arc;
+//use std::sync::Arc;
 
 
 pub struct Node {
@@ -15,10 +15,31 @@ pub struct Node {
 
 impl Node {
     
-    pub fn new(id:String, address: SocketAddr) => Self {
+    pub fn new(id: String, address: SocketAddr) -> Node {
         // Init Constructor here
+        let (sender, mut receiver) = mpsc::channel(1);
+
+        let mut node = Node {
+            id: id,
+            address: address,
+            peers: Mutex::new(HashSet::new()),
+            sender: sender,
+            receiver: receiver,
+        };
+        node
     }
 
+    
+    pub fn get_id(&self) -> String {
+        return (self.id).clone();
+    }
+
+    pub fn get_addr(&self) -> SocketAddr {
+        return (self.address).clone();
+    }
+
+
+    /*
     pub async fn connect_to_peer(&self, peer_addr: SocketAddr) {
         
     }
@@ -39,18 +60,22 @@ impl Node {
     fn handle_message(&self, message: String) {
         
     }
+    */
 
-    fn add_peer(&self, peer_addr: SocketAddr) {
-
+    fn async add_peer(&self, peer_addr: SocketAddr) {
+        let mut peers = self.peers.lock().await;
+        peers.insert(peer_addr);
     }
 
     fn remove_peer(&self, peer_addr: SocketAddr) {
-        
+        let mut peers = self.peers.lock().await;
+        peers.remove(&addr);
     }
 
+    /*
     pub async fn discover_peers(&self) {
         
     }
-
+    */
 
 }
---
+++ tests/node_test.rs
@@ -0,0 +1,15 @@
+use chat_app::node::node::Node;
+use std::net::{IpAddr, Ipv4Addr};
+use std::net::SocketAddr; 
+
+
+#[test]
+fn node_instantiation() {
+    let node_id = String::from("node1");
+    let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
+    let node = Node::new(node_id.clone(), socket);
+
+    assert_eq!(node.get_id(), node_id);
+    assert_eq!(node.get_addr(), socket);
+}
+
